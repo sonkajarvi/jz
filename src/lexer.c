dbaeaeebf1ecac2edd24c37c67b24964ca92a67f
sonkajarvi <sonkajarvi@hotmail.com>
2025-01-19 08:24:53 +0200

    lexer: Add utf8_to_codepoint()

--- src/lexer.c
+++ src/lexer.c
@@ -34,6 +34,62 @@ static int read(struct lexer *lx)
     return c;
 }
 
+int utf8_to_codepoint(const char *bytes, int *size)
+{
+    int retval;
+
+    if (!bytes)
+        return -1;
+
+#define A(i, m, x) ((bytes[i] & m) == x)
+#define B(i, m, x) ((bytes[i] & m) << x)
+
+    // 0xxx xxxx
+    if (A(0, 0x80, 0x0)) {
+        if (size)
+            *size = 1;
+        return bytes[0];
+    }
+
+    // 110xx xxx 10xx xxxx
+    if (A(0, 0xe0, 0xc0) && A(1, 0xc0, 0x80)) {
+        retval = B(0, 0x1f, 6) | B(1, 0x3f, 0);
+        if (retval < 0x80)
+            return -1;
+
+        if (size)
+            *size = 2;
+        return retval;
+    }
+
+    // 1110 xxxx 10xx xxxx 10xx xxxx
+    if (A(0, 0xf0, 0xe0) && A(1, 0xc0, 0x80) && A(2, 0xc0, 0x80)) {
+        retval = B(0, 0xf, 12) | B(1, 0x3f, 6) | B(2, 0x3f, 0);
+        if (retval < 0x800 || (retval >= 0xd800 && retval <= 0xdfff))
+            return -1;
+
+        if (size)
+            *size = 3;
+        return retval;
+    }
+
+    // 1111 0xxx 10xx xxxx 10xx xxxx 10xx xxxx
+    if (A(0, 0xf8, 0xf0) && A(1, 0xc0, 0x80) && A(2, 0xc0, 0x80)  && A(3, 0xc0, 0x80)) {
+        retval = B(0, 0x7, 18) | B(1, 0x3f, 12) | B(2, 0x3f, 6) | B(3, 0x3f, 0);
+        if (retval < 0x10000 || retval > 0x10ffff)
+            return -1;
+
+        if (size)
+            *size = 4;
+        return retval;
+    }
+
+#undef A
+#undef B
+
+    return -1;
+}
+
 static void skip_whitespace(struct lexer *lx)
 {
     while (is_whitespace(peek(lx)))
